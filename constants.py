995992ca6ed4f16dedb545708261c349a421a47e
duongvu6 <vucongtuanduong6@gmail.com>
2025-08-29 00:08:45 +0000

    add thresholding

---
+++ constants.py
@@ -0,0 +1,3 @@
+NEGATIVE_IMAGE = "Negative image"
+THRESHOLDING = "Thresholding"
+THRESHOLD_VALUE = "threshold_value"
--- image_enhancement/image_enhancement.py
+++ image_enhancement/image_enhancement.py
@@ -7,3 +7,11 @@ def negative_image(image: Image.Image):
     out = 1.0 - arr
     out_img = Image.fromarray((np.clip(out, 0.0, 1.0) * 255.0).round().astype(np.uint8), mode="L")
     return out_img
+
+def thresholding(image: Image.Image, threshold_value: int):
+    gray = image.convert("L")
+    arr = np.asarray(gray).astype(np.float32)
+    arr[arr < threshold_value] = 0
+    arr[arr >= threshold_value] = 255
+    out_img = Image.fromarray(arr.astype(np.uint8), mode="L")
+    return out_img
